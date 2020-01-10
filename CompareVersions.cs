94f0540269d2edb2f9a27891c0489de2f4cb4f17
Diana Novitska <Diana_Novitska@epam.com>
2020-01-10 17:12:29 +0200

    Add project files.

---
+++ CompareVersions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareVersion
+{
+    class CompareVersions
+    {
+        public string ResultOfComparison(string s, string s2)
+        {
+
+            string[] versionA = s.Split(".");
+            string[] versionB = s2.Split(".");
+            int j; int i;
+            String HigherVersion = "";
+            string[] longerVersion;
+
+            int size = (versionA.Length < versionB.Length) ? versionA.Length : versionB.Length;
+            longerVersion = (versionA.Length > versionB.Length) ? versionA : versionB;
+            string longerVersionText = (versionA.Length > versionB.Length) ? "VersionA" : "VersionB";
+
+            for (i = 0, j = 0; i < size; i++, j++)
+            {
+                int isEqual = versionA[i].CompareTo(versionB[i]);
+                switch (isEqual)
+                {
+                    case -1:
+                        HigherVersion = "versionB is higher";
+                        break;
+                    case 1:
+                            HigherVersion = "versionA is higher";
+                            break;                     
+                    case 0:
+                        {
+                            HigherVersion = "Versions are equal";
+                            if (versionA.Length != versionB.Length)
+                            {
+                                if ((longerVersion[size].Equals("0")))
+                                {
+                                    HigherVersion = "Versions are equal";
+                                }
+                                else
+                                {
+                                    HigherVersion = longerVersionText + " is higher";
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Wrong format of strings");
+                        break;
+
+
+                }
+            }
+
+            return HigherVersion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using MySqlX.XDevAPI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CompareVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompareVersions c = new CompareVersions();
+            Console.WriteLine(c.ResultOfComparison("1.2", "1.2"));
+            Console.WriteLine(  c.ResultOfComparison("1.3", "1.2"));
+            Console.WriteLine(  c.ResultOfComparison("1.2", "1.5"));
+            Console.WriteLine(  c.ResultOfComparison("1.2.0", "1.2"));
+            Console.WriteLine(  c.ResultOfComparison("1.2", "1.2.0"));
+            Console.WriteLine(  c.ResultOfComparison("1.2.9.8.1", "1.3"));
+            Console.WriteLine(  c.ResultOfComparison("1.3", "1.2.9.8"));
+            Console.WriteLine(c.ResultOfComparison("1.2.9", "1.2"));
+            Console.WriteLine(c.ResultOfComparison("1.2", "1.2.9"));
+        } 
+    }
+}
